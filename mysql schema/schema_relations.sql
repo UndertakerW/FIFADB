e5cce35b290246b6772c5da622f139dfe492d262
AAAAgito <1051450352@qq.com>
2021-04-14 23:10:36 +0800

    Revert "Update schema_relations.sql"
    This reverts commit 62634ecb2cf8bc7d3eb7cf46e92021289f41d5ac.

--- mysql schema/schema_relations.sql
+++ mysql schema/schema_relations.sql
@@ -1,8 +1,5 @@
-
-drop SCHEMA if exists FIFADB;
 CREATE SCHEMA FIFADB;
 USE FIFADB;
-drop table if exists player;
 CREATE TABLE player
 (
 player_id varchar(20),
@@ -12,7 +9,7 @@ date_of_birth varchar(10),
 nationality varchar(20),
 height varchar(5),
 weight varchar(5),
-club_id varchar(20),
+club_id varchar(10),
 loaned_fron varchar(20),
 wage_eur varchar(10),
 value_eur varchar(20),
@@ -30,16 +27,12 @@ tag varchar(10),
 primary key(player_id,season)
 );
 
-
-CREATE TABLE player_dob_info
-(
-player_id varchar(20) unique,
-date_of_birth varchar(10),
-primary key(player_id)
-);
-
-create table country(
-country_name varchar(10) primary key
+CREATE TABLE CLUB(
+club_id varchar(10),
+season varchar(5),
+club_name varchar(40),
+league_id varchar(40),
+primary key(club_id,season)
 );
 
 CREATE TABLE LEAGUE(
@@ -47,20 +40,13 @@ league_id varchar(10),
 league_name varchar(40),
 league_rank varchar(5),
 country varchar(10),
-primary key (league_id),
-foreign key(country) references country(country_name)
+primary key (league_id)
 );
 
-CREATE TABLE CLUB(
-club_id varchar(20),
-season varchar(5),
-club_name varchar(40),
-league_id varchar(40),
-primary key(club_id,season),
-foreign key(league_id) references league(league_id)
+create table country(
+country_name varchar(10) primary key
+
 );
-
-
 
 
 
@@ -74,7 +60,20 @@ passing varchar(5),
 dribbling varchar(5),
 defending varchar(5),
 physic varchar(5),
-primary key(player_id,season),
+foreign key (player_id,season) references player(player_id,season)
+);
+
+
+CREATE TABLE general_player
+(
+player_id varchar(20),
+season varchar(5),
+pace varchar(5),
+shooting varchar(5),
+passing varchar(5),
+dribbling varchar(5),
+defending varchar(5),
+physic varchar(5),
 foreign key (player_id,season) references player(player_id,season)
 );
 
@@ -88,7 +87,6 @@ gk_kicking varchar(5),
 gk_reflexes varchar(5),
 gk_speed varchar(5),
 gk_positioning varchar(5),
-primary key(player_id,season),
 foreign key (player_id,season) references player(player_id,season)
 );
 
@@ -104,13 +102,66 @@ tag_name varchar(10) primary key
 );
 
 
+create table player_club
+(
+player_id varchar(20),
+season varchar(5),
+club_id varchar(10) not null,
+joined_time varchar(10),
+primary key(player_id,season,club_id),
+foreign key(player_id,season) references player(player_id,season),
+foreign key(club_id) references club(club_id)
+
+);
+
+create table player_loan_from
+(
+player_id varchar(20),
+season varchar(5),
+club_id varchar(10),
+primary key(player_id,season,club_id),
+foreign key(player_id,season) references player(player_id,season),
+foreign key(club_id) references club(club_id)
+);
+
+
+create table club_league
+(
+league_id varchar(10) not null,
+season varchar(5),
+club_id varchar(10),
+primary key(league_id,season,club_id),
+foreign key(league_id) references league(league_id),
+foreign key(club_id,season) references club(club_id,season)
+
+);
+
+create table league_country
+(
+league_id varchar(10),
+country_name varchar(10) not null,
+primary key(league_id,country_name),
+foreign key(league_id) references league(league_id),
+foreign key(country_name) references country(country_name)
+);
+
+create table player_country
+(
+player_id varchar(20),
+season varchar(5),
+country_name varchar(10) not null,
+primary key(player_id,season,country_name),
+foreign key(player_id,season) references player(player_id,season),
+foreign key(country_name) references country(country_name)
+);
+
 
 create table player_best_position
 (
 player_id varchar(20),
 season varchar(5),
 position_name varchar(10),
-primary key(player_id,season),
+primary key(player_id,season,position_name),
 foreign key(player_id,season) references player(player_id,season),
 foreign key(position_name) references positions(position_name)
 );
@@ -139,7 +190,5 @@ foreign key(player_id,season) references player(player_id,season),
 foreign key(tag_name) references tag(tag_name)
 );
 
-alter table player add foreign key(nationality) references country(country_name);
-alter table player add foreign key(club_id) references club(club_id);
 commit
 
