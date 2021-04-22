0356bbed1b4bb994f0b22e24b53c5b3bd71ccf9c
Sylviachelsea <43447503+Sylviachelsea@users.noreply.github.com>
2021-04-23 03:47:40 +0800

    update website

--- Codes/step_1_construct_tables.sql
+++ Codes/step_1_construct_tables.sql
@@ -3,7 +3,27 @@ begin;
 drop SCHEMA if exists FIFADB;
 CREATE SCHEMA FIFADB;
 USE FIFADB;
-
+select distinct league_id,league_name from league;
+select * from league where league_id=3;
+CREATE TABLE Admin
+(
+	ID varchar(10) primary key,
+    pwd varchar(20),
+    name varchar(10),
+    email varchar(10)
+);
+create table new_player(
+player_id varchar(10),
+season varchar(5),
+player_name varchar (255),
+nationality varchar(255),
+club_id varchar(10),
+overall varchar(3),
+potential varchar(3),
+primary key(player_Id,season)
+);
+show tables;
+insert into admin values('admin','123456','zsq','118010416@');
 CREATE TABLE player
 (
 player_id varchar(10),
