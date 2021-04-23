18b4ae43e6f5cd0160a78fa06ac389f2d5051d7c
MoyuST <46509809+MoyuST@users.noreply.github.com>
2021-04-23 13:59:31 +0800

    create a dedicated website table
    create a dedicated website table contains player.*, club.club_name, plauer_best_position_position.position_name

--- Codes/step_6_website_database.sql
+++ Codes/step_6_website_database.sql
@@ -1,11 +1,13 @@
-CREATE TABLE Admin
+create table Admin
 (
 	ID varchar(10) primary key,
     pwd varchar(20),
     name varchar(10),
     email varchar(10)
 );
-CREATE TABLE new_player(
+insert into admin values('admin','123456','zsq','118010416@');
+
+create table new_player(
 player_id varchar(10),
 season varchar(5),
 player_name varchar (255),
@@ -15,4 +17,9 @@ overall varchar(3),
 potential varchar(3),
 primary key(player_Id,season)
 );
-insert into admin values('admin','123456','zsq','118010416@');
+
+create table website_table 
+	select player.*, club.club_name, player_best_position.position_name
+    from player natural join club natural join player_best_position;
+    
+alter table website_table add primary key (player_id, season, position_name);
